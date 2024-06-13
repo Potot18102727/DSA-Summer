32483f62261feb4ade75d6aed603ce231030d03c
Rixielie Marie Potot <theartp1@gmail.com>
2024-06-14 00:14:14 +0800

    Initial Commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "StackArray.h"
+
+
+
+int main(int argc, char *argv[]) {
+    
+    StackArrayList sList;
+    initStack(&sList);
+    display(sList);
+    
+    
+    
+    return 0;
+}
